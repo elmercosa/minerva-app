28fad4d64a2f04362a7695439c76f42f0f2fa0c6
Elmer Cortez <UO257192@uniovi.es>
2024-01-03 17:12:25 +0100

    nextui

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,30 +1,31 @@
-import { createClient } from '@/utils/supabase/server'
-import Link from 'next/link'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
+import { cookies } from "next/headers";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/utils/supabase/server";
 
 export default async function AuthButton() {
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
 
   const {
     data: { user },
-  } = await supabase.auth.getUser()
+  } = await supabase.auth.getUser();
 
   const signOut = async () => {
-    'use server'
+    "use server";
 
-    const cookieStore = cookies()
-    const supabase = createClient(cookieStore)
-    await supabase.auth.signOut()
-    return redirect('/login')
-  }
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+    await supabase.auth.signOut();
+    return redirect("/login");
+  };
 
   return user ? (
     <div className="flex items-center gap-4">
       Hey, {user.email}!
       <form action={signOut}>
-        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+        <button className="px-4 py-2 no-underline rounded-md bg-btn-background hover:bg-btn-background-hover">
           Logout
         </button>
       </form>
@@ -32,9 +33,9 @@ export default async function AuthButton() {
   ) : (
     <Link
       href="/login"
-      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+      className="flex px-3 py-2 no-underline rounded-md bg-btn-background hover:bg-btn-background-hover"
     >
       Login
     </Link>
-  )
+  );
 }
