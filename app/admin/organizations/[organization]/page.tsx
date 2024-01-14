87f29a4ce79288225886a3d3176c68b64b0a2f8f
Elmer José Cortez Sanjinez <35766169+elmercosa@users.noreply.github.com>
2024-01-14 20:24:11 +0100

    add organization title

--- app/admin/organizations/[organization]/page.tsx
+++ app/admin/organizations/[organization]/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import useUser from "@/hooks/useUser";
+
 export default function Page({ params }: { params: { organization: string } }) {
-  return <div>My Post: {params.organization}</div>;
+  const User: any = useUser();
+
+  return (
+    <div className="flex">
+      <div className="flex flex-col items-center justify-center w-full gap-2">
+        <span className="text-xl font-bold animate-in">Bienvenido a</span>
+        <h1 className="text-6xl font-bold animate-in">
+          {User.data?.organization.name}
+        </h1>
+      </div>
+    </div>
+  );
 }
